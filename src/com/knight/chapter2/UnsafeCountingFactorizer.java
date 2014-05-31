fda1d7f70fb3beb3358b79c296f498032e8e8708
knightoning <hujie20081122@163.com>
2014-05-31 19:08:05 +0800

    修改

--- src/com/knight/chapter2/UnsafeCountingFactorizer.java
+++ src/com/knight/chapter2/UnsafeCountingFactorizer.java
@@ -9,6 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+/**
+ * 存在一个共享的访问资源count变量
+ * ++ count 不是原子性操作，分3步，先取值（读），再自增，最后赋值（写） 读-改-写
+ * 多个线程存在资源的竞争，假设初始9，同时加1，同时变成10，显然违背了请求一次自增一次的原则
+ * @author hujie
+ *
+ */
 public class UnsafeCountingFactorizer  implements Servlet{
 
 	private long count = 0;
